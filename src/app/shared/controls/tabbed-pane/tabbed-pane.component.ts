d9dbf0983e5ea41774d0972694676ebc8e8fc83e
Alex Thalhammer <alex@angular-software.at>
2022-01-23 16:37:13 +0100

    01_c: Bonus: Directly Accessing a ViewChild *

--- src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
+++ src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -1,25 +1,63 @@
-import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ContentChildren,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
+import { TabNavigatorComponent } from '../tab-navigator/tab-navigator.component';
 
 @Component({
   selector: 'app-tabbed-pane',
   templateUrl: './tabbed-pane.component.html',
   styleUrls: ['./tabbed-pane.component.scss']
 })
-export class TabbedPaneComponent implements AfterContentInit {
-  @ContentChildren(TabComponent)
-  tabQueryList: QueryList<TabComponent> | undefined;
+export class TabbedPaneComponent implements OnInit, AfterContentInit, AfterViewInit {
+  // @ContentChild(TabComponent)
+  // tabComponent?: TabComponent;
 
-  activeTab: TabComponent | undefined;
+  @ContentChildren(TabComponent)
+  tabQueryList?: QueryList<TabComponent>;
+
+  @ViewChild('navigator')
+  navigator?: TabNavigatorComponent;
+
+  // @ViewChildren(TabNavigatorComponent)
+  // navigators?: QueryList<TabNavigatorComponent>;
+
+  activeTab?: TabComponent;
   currentPage = 1;
 
   get tabs(): TabComponent[] {
     return this.tabQueryList?.toArray() ?? [];
   }
 
+  ngOnInit(): void {
+    console.log(this.tabs);
+    console.log(this.navigator);
+  }
+
   ngAfterContentInit(): void {
     if (this.tabs.length > 0) {
+      console.log(this.tabs);
       this.activate(this.tabs[0]);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.navigator) {
+      console.log(this.navigator);
+      this.navigator.pageCount = this.tabs.length;
+      // This line would cause a cycle:
+      // this.navigator.page = 1;
+      this.navigator.pageChange.subscribe((page: number) => {
+        this.pageChange(page);
+      });
     }
   }
 
