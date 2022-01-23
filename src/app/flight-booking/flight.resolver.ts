147d62086789f7a16ba3438ea3eb02cc8a81a0c2
Alex Thalhammer <alex@angular-software.at>
2022-01-23 23:04:18 +0100

    05_r: Bonus: Loading Indicator *

--- src/app/flight-booking/flight.resolver.ts
+++ src/app/flight-booking/flight.resolver.ts
@@ -3,6 +3,7 @@ import { Flight } from './flight';
 import { FlightService } from './flight.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class FlightResolver {
@@ -10,6 +11,6 @@ export class FlightResolver {
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Flight> {
     const id = route.params.id;
-    return this.flightService.findById(id);
+    return this.flightService.findById(id).pipe(delay(3000)); // <-- delay!
   }
 }
