8abcadda767bc8598e1c2147229249dfec07e8f9
Alex Thalhammer <alex@angular-software.at>
2022-01-23 16:19:55 +0100

    01_c: Content Projection & Referencing the Parent Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,19 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { DateComponent } from './date/date.component';
 import { CityPipe } from './pipes/city.pipe';
 import { StatusColorPipe } from './pipes/status-color.pipe';
 import { StatusFilterPipe } from './pipes/status-filter.pipe';
-import { FormsModule } from '@angular/forms';
+
+import { TabbedPaneComponent } from './controls/tabbed-pane/tabbed-pane.component';
+import { TabComponent } from './controls/tab/tab.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [DateComponent, CityPipe, StatusColorPipe, StatusFilterPipe],
-  exports: [DateComponent, CityPipe, StatusColorPipe, StatusFilterPipe, CommonModule, FormsModule]
+  declarations: [DateComponent, CityPipe, StatusColorPipe, StatusFilterPipe, TabbedPaneComponent, TabComponent],
+  exports: [CommonModule, FormsModule, DateComponent, CityPipe, StatusColorPipe, StatusFilterPipe, TabbedPaneComponent, TabComponent]
 })
 export class SharedModule {}
