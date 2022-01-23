57158399edf0fc9bc81552af420ee73dc1757ad5
Alex Thalhammer <alex@angular-software.at>
2022-01-23 16:44:25 +0100

    01_c: Bonus: Communicating via a Service *

--- src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
+++ src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 import { TabNavigatorComponent } from '../tab-navigator/tab-navigator.component';
+import { TabbedPaneService } from './tabbed-pane.service';
 
 @Component({
   selector: 'app-tabbed-pane',
@@ -33,6 +34,8 @@ export class TabbedPaneComponent implements OnInit, AfterContentInit, AfterViewI
   activeTab?: TabComponent;
   currentPage = 1;
 
+  constructor(private service: TabbedPaneService) {}
+
   get tabs(): TabComponent[] {
     return this.tabQueryList?.toArray() ?? [];
   }
@@ -50,15 +53,14 @@ export class TabbedPaneComponent implements OnInit, AfterContentInit, AfterViewI
   }
 
   ngAfterViewInit(): void {
-    if (this.navigator) {
-      console.log(this.navigator);
-      this.navigator.pageCount = this.tabs.length;
-      // This line would cause a cycle:
-      // this.navigator.page = 1;
-      this.navigator.pageChange.subscribe((page: number) => {
-        this.pageChange(page);
-      });
-    }
+    this.service.pageCount.next(this.tabs.length);
+    this.service.currentPage.subscribe((page: number) => {
+      // Prevent cycle:
+      if (page === this.currentPage) {
+        return;
+      }
+      this.pageChange(page);
+    });
   }
 
   register(tab: TabComponent): void {
@@ -70,6 +72,9 @@ export class TabbedPaneComponent implements OnInit, AfterContentInit, AfterViewI
       tab.visible = tab === active;
     }
     this.activeTab = active;
+    // Update:
+    this.currentPage = this.tabs.indexOf(active) + 1;
+    this.service.currentPage.next(this.currentPage);
   }
 
   pageChange(page: number): void {
