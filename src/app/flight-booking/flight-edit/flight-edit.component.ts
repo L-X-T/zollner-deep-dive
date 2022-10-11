0f59eded55b810d92f266ff6afa23149f09d6cd9
Alex Thalhammer <alex@angular-software.at>
2022-10-11 12:04:30 +0200

    Demo: Routing subscription and type conversions

--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,7 +1,8 @@
 // src/app/flight-booking/flight-edit/flight-edit.component.ts
 
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-flight-edit',
@@ -12,12 +13,31 @@ export class FlightEditComponent implements OnInit {
   id = 0;
   showDetails = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((p) => {
-      this.id = p.id;
-      this.showDetails = p.showDetails;
+    console.log(this.router.url);
+    console.log(this.route.snapshot);
+    this.id = this.route.snapshot.params.id;
+    this.showDetails = this.route.snapshot.params.showDetails;
+
+    this.route.paramMap.pipe(delay(2000)).subscribe((p) => {
+      console.log('route params received:', p);
+
+      this.showDetails = p.get('showDetails') === 'true';
+      console.log('showDetails set to:', this.showDetails);
+
+      this.id = Number(p.get('id'));
+      const idAsString = '' + this.id;
+      console.log('number:', this.id);
+      console.log('string:', idAsString);
     });
+  }
+
+  onRoute(): void {
+    const showDetails = !this.showDetails;
+    this.router
+      .navigate(['flight-booking', 'flight-edit', this.id, { showDetails }])
+      .then(() => console.log('navigated to showDetails:', showDetails));
   }
 }
